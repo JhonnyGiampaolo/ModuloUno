344400287658332a5258fc3c395ffe6766036afc
JhonnyGiampaolo <neogiampaolo@gmail.com>
2020-11-27 10:54:49 -0500

    Finalización del Taller 2 de Java de la empresa TCS

--- src/com/clases/BeneficiosCovid19.java
+++ src/com/clases/BeneficiosCovid19.java
@@ -1,5 +1,7 @@
 package com.clases;
 
+import java.util.ArrayList;
+
 public class BeneficiosCovid19 {
     //Debe ser el numero entero aleatorio puedes usar la funcion ramdom para esto
     private String id;
@@ -8,8 +10,29 @@ public class BeneficiosCovid19 {
 
     //Completar clase
 
-    //Implementar un metodo que retorne el mejor beneficio comparando los valores de todos los beneficios
-    public Float getMejorbeneficios(){
-        return Float.valueOf(0);
+
+    public String getId() {
+        return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Float getValorSubsidio() {
+        return valorSubsidio;
+    }
+
+    public void setValorSubsidio(String valorSubsidio) {
+        this.valorSubsidio = Float.parseFloat(valorSubsidio);
+    }
+
 }
--- src/com/clientes/Main.java
+++ src/com/clientes/Main.java
@@ -1,13 +1,21 @@
 package com.clientes;
 
+import com.clases.BeneficiosCovid19;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import javax.xml.crypto.Data;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Math.random;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code her
+    static final double TOPLIMIT = 999999;
 
+    public static void main(String[] args) {
         /**
          * Crear 2 lista de beneficios para clientes Lista 1 y Lista 2 por ejemplo
          * Ejemplo:
@@ -21,24 +29,78 @@ public class Main {
          * valorSubsidio:200000
          */
 
+        ArrayList<BeneficiosCovid19> objectList = new ArrayList();
+        BeneficiosCovid19 object = new BeneficiosCovid19();
+        Scanner input = new Scanner(System.in);
+        String cantidad = null;
+        String money = null;
+        Integer iteractions = 0;
+        boolean goOn = true;
 
+        while (goOn) {
+            System.out.println("Bienvenido al sistema de registro de Beneficios del Covid 19");
+            System.out.print("Cuantos beneficios deseas ingresar: ");
+            cantidad = input.nextLine();
+            goOn = !(isNumeric(cantidad));
+        }
+        iteractions = Integer.parseInt(cantidad);
 
-
+        for (int i = 1; i <= iteractions; i++) {
+            boolean follow = false;
+            object.setId(getIdBeneficio());
+            System.out.println("El ID del beneficio Nro "+i+" que estas creando es: "+object.getId());
+            System.out.print("Escribe el nombre del beneficio que estas creando: ");
+            object.setNombre(input.nextLine());
+            while(!(follow)){
+                System.out.print("Escribe el valor monetario de este beneficio: ");
+                money = input.nextLine();
+                follow = (isNumeric(money));
+            }
+            object.setValorSubsidio(money);
+            objectList.add(object);
+        }
+        getMejorBeneficio(objectList);
     }
 
-    /**
-    * Implementa un metodo que entregue un reporte de comparacion de las 2 listas este metodo solo imprimira regiostros
-    * Cuando un metodo no retorna un tipo de datos se trabaja con la palabra clave VOID
-    * Abarcar los operadores terniarios para condiciones logicas
-    * Operadores logicos
-    * Operacionres aritmeticos
-    **/
+    private static String getIdBeneficio(){
+        Double number =  Math.random()*TOPLIMIT;
+        Integer id = number.intValue();
+        return id.toString();
+    }
 
+    public static boolean isNumeric(String value) {
+        boolean validation;
 
+        try {
+            Double.parseDouble(value);
+            validation = true;
+        }catch (Exception e){
+            System.out.println("Has ingresado un valor incorrecto.");
+            System.out.println("Debes ingresar un numero");
+            validation = false;
+        }
+        return validation;
+    }
 
-    //Implementa un metodo que te entregue un numero aletorio para el ide de los beneficios usar el wrapper de Integer
-    private String getIdBeneficio(){
-        //La Clase Math tiene varios metodos que te ayudaran
-        return"";
+    public static void  getMejorBeneficio (ArrayList<BeneficiosCovid19> list) {
+        BeneficiosCovid19 element = new BeneficiosCovid19();
+        Float value = Float.valueOf(0);
+
+        for (int j = 0; j < list.size(); j++) {
+            element = list.get(j);
+            Float elementValue = element.getValorSubsidio();
+            if (value < elementValue) {
+                value = elementValue;
+                element = list.get(j);
+            }
+        }
+        showContent(element);
+    }
+
+    public static void showContent(BeneficiosCovid19 list){
+        System.out.println("El mejor beneficio de los que creaste es el siguiente: ");
+        System.out.println("ID: "+list.getId());
+        System.out.println("Nombre: "+list.getNombre());
+        System.out.println("El Valor del subsidio es: "+list.getValorSubsidio());
     }
 }
